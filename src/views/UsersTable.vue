465383e4af491551bbbbe3281d2196e43337700b
getScaled-dev <tayyab@getscaled.com>
2023-05-31 18:09:41 +0000

    fix file download

--- src/views/UsersTable.vue
+++ src/views/UsersTable.vue
@@ -399,7 +399,7 @@ export default {
     exportCSV() {
       this.exportLoader = true;
       // let pagination = `?itemsPerPage=${this.pagination.itemPerPage}&page=${this.pagination.page}`
-      let filters = {
+       let filters = {
         lastName: this.filtersData?.lastName || null,
         firstName: this.filtersData?.firstName || null,
         age: this.filtersData?.age || null,
@@ -416,24 +416,23 @@ export default {
 
         companyName: this.filtersData?.companyName || null,
         jobTitle: this.filtersData?.jobTitle || null,
+        jobTitleValue: this.filtersData?.jobTitles || null,
         dob: this.filtersData?.dob || null,
         firstNameValue: this.filtersData?.firstNameValue || null,
         lastNameValue: this.filtersData?.lastNameValue || null,
         ageValue: this.filtersData?.ageValue || null,
-        cityValue: this.filtersData?.cityValue || null,
+        cityValue: this.filtersData?.cities || null,
         dobValue: this.filtersData?.dobvalue || null,
         addressValue: this.filtersData?.addressValue || null,
         address2Value: this.filtersData?.address2Value || null,
         emailValue: this.filtersData?.emailValue || null,
         companyPhoneValue: this.filtersData?.companyPhoneValue || null,
-        companyNameValue: this.filtersData?.companyNameValue || null,
-        jobTitleValue: this.filtersData?.jobTitleValue || null,
+        companyNameValue: this.filtersData?.companies || null,
+
         stateValue: this.filtersData?.stateValue || null,
         zipCode: this.filtersData?.zipCodes || null,
       };
-      let url = `${
-        process.env.VUE_APP_API_URL
-      }api/dashboard?export=${true}&ageStartValue=${
+        let url = `${process.env.VUE_APP_API_URL}api/dashboard?export=${true}&ageStartValue=${
         filters?.ageStartValue
       }&ageEndValue=${filters?.ageEndValue}&age=${filters?.age}&firstName=${
         filters?.firstName
@@ -447,15 +446,15 @@ export default {
         filters?.mobilePhone
       }&mobilePhoneValue=${filters?.mobilePhoneValue}&city=${
         filters?.city
-      }&cityValue=${filters?.cityValue}&state=${filters?.state}&stateValue=${
-        filters?.stateValue
-      }&jobTitle=${filters?.jobTitle}&jobTitleValue=${
-        filters?.jobTitleValue
-      }&address=${filters?.address}&addressValue=${
-        filters?.addressValue
-      }&companyName=${filters?.companyName}&companyNameValue=${
-        filters?.companyNameValue
-      }`;
+      }&cityValue=${JSON.stringify(filters?.cityValue)}&state=${
+        filters?.state
+      }&stateValue=${filters?.stateValue}&jobTitle=${
+        filters?.jobTitle
+      }&jobTitleValue=${JSON.stringify(filters?.jobTitleValue)}&address=${
+        filters?.address
+      }&addressValue=${filters?.addressValue}&companyName=${
+        filters?.companyName
+      }&companyNameValue=${JSON.stringify(filters?.companyNameValue)}`;
 
       axios
         .get(url, {
