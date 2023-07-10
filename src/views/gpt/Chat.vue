e67c82fe908a9b56f2ce37d44150f5cbd566eeb6
getScaled-dev <tayyab@getscaled.com>
2023-07-10 21:46:39 +0000

    worked on dob filter

--- src/views/gpt/Chat.vue
+++ src/views/gpt/Chat.vue
@@ -1,26 +1,79 @@
 <template>
-  <div class="d-flex justify-space-between">
-    <div>
- <v-text-field
-              class="mt-5"
-              outlined
-              type="text"
-            
-              v-model="gptPrompt"
-            ></v-text-field>
+  <div class="d-flex justify-spac-between">
+    <div style="width: 20%">
+      <h4>Previous Chats</h4>
+      <v-divider></v-divider>
+      <div class="mt-3 mb-3 d-flex justify-space-between">
+        <div>Write 10 Emails for...</div>
+        <div>
+          <v-btn small color="rgb(7, 2, 83)" dark>View</v-btn>
+        </div>
+      </div>
+      <v-divider></v-divider>
+      <div class="mt-3 mb-3 d-flex justify-space-between">
+        <div>Write 10 Emails for...</div>
+        <div>
+          <v-btn small color="rgb(7, 2, 83)" dark>View</v-btn>
+        </div>
+      </div>
+      <v-divider></v-divider>
     </div>
-    <div>
-        gpt response
+    <v-divider vertical class="ml-2" inset></v-divider>
+    <div style="width: 70%" class="ml-8">
+      <v-card style="height: 80%;" class="mb-4">
+        <v-expand-transition>
+          <v-card
+            class="transition-fast-in-fast-out v-card--reveal"
+            style="height: 100%"
+            color="blue-grey lighten-5"
+          >
+            <v-card-text class="pb-0">
+            
+              <p style="color: black">
+                late 16th century (as a noun denoting a place where alms were
+                distributed): from medieval Latin eleemosynarius, from late
+                Latin eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’
+              </p>
+            </v-card-text>
+          </v-card>
+        </v-expand-transition>
+      </v-card>
+      <h4 class="mb-3">What would you like to work on?</h4>
+      <v-textarea
+        outlined
+        name="input-7-4"
+        label="Write Prompt Here..."
+        v-model="gptPrompt"
+      ></v-textarea>
+      <div class="d-flex justify-space-between">
+        <div>
+          <v-btn small color="rgb(7, 2, 83)" dark>Submit to chat GPT</v-btn>
+          <v-btn small class="ml-3" @click="gptPrompt = ''">Clear</v-btn>
+          <v-btn small class="ml-3" @click="gptPrompt = ''">Save</v-btn>
+
+        </div>
+        <div>
+          <v-btn small color="rgb(215, 93, 63)" dark
+            >Approve and send to GetScaled account</v-btn
+          >
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      gptPrompt: "",
+    };
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+::v-deep .v-input__slot {
+  background: white !important;
+}
 </style>
